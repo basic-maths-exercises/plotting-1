e89cd0de5095ed7d417f4f04cee2c22a943675a5
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-05 16:40:20 +0100

    Added new tests

---
+++ testing/test_main.py
@@ -0,0 +1,22 @@
+try:
+    import AutoFeedback.plotchecks as pc
+    from AutoFeedback.plotclass import line
+except:
+    import subprocess
+    import sys
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
+    import AutoFeedback.plotchecks as pc
+    from AutoFeedback.plotclass import line
+
+import unittest
+from main import *
+
+xvals = np.linspace(0,29,30), 
+line1 = line(xvals,xvals*xvals)
+
+axislabels=["Index", "Square"]
+
+class UnitTests(unittest.TestCase) :
+    def test_fib(self) :
+       assert( pc.check_plot([line1],explabels=axislabels,explegend=False,output=True) )    
