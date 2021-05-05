4521a15bae9169dd6a754b02a1a000a2e99e35bc
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-05 16:45:59 +0100

    Setup answers

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+xvals = np.linspace(0,29,30)
+yvals = xvals*xvals 
+
+plt.plot( xvals, yvals, 'ko'  )
+plt.xlabel("Index")
+plt.ylabel('Square')
+plt.savefig("squares.png")
